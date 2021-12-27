482f8267b86b6eee17ad38fab3de33bf3975139f
chinmay <chinmay11.moghe@gmail.com>
2021-12-27 20:41:15 +0530

    Added styled components

--- src/globalStyle.ts
+++ src/globalStyle.ts
@@ -1,13 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*,*::after,*::before {
+  margin:0;
+  padding:0;
+}
+
+html {
+  font-size: 62.5%;
+  letter-spacing: 0.1rem;
+}
+
 body {
-  margin: 0;
+  font-size: 1.6rem;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
-
-code {
-  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-    monospace;
-}
+`;
+export default GlobalStyle;
