590cf366f9af9462bae03ffdd6152f2bd1027089
chinmay <chinmay11.moghe@gmail.com>
2021-12-30 10:10:13 +0530

    add audio track and player component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css'
+import Canvas from './components/Canvas';
+import AudioPlayer from './components/AudioPlayer';
+import FeelGood from './assets/Syn Cole - Feel Good [NCS Release].webm';
 
 function App() {
+  const draw = (ctx:CanvasRenderingContext2D)=>{};
   return (
     <div className="App">
-      Audio visualizer
+      <Canvas draw={draw} width={400} height={400}/>
+      <AudioPlayer track={FeelGood}/>
     </div>
   )
 }
